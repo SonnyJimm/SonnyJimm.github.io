de82275236352e9171fb51e27bc20899ebfa5942
SonnyJimm <saraasoulmate@gmail.com>
2023-01-23 03:32:08 -0600

    fixed lint error

--- jsdec/ascii.js
+++ jsdec/ascii.js
@@ -6,60 +6,60 @@ let interVal = 250;
 let index = 0;
 
 // stores selected frame;
-let frame=[];
+let frame = [];
 // init values on page loads
 window.onload = () => {
-  "use strict";
+  'use strict';
   // put all of your code here
-  document.getElementById("start").onclick = onstart;
-  document.getElementById("stop").onclick = onstop;
-  document.getElementById("fontsize").onchange = onfontchange;
-  document.getElementById("turbo").onchange = onturbo;
+  document.getElementById('start').onclick = onstart;
+  document.getElementById('stop').onclick = onstop;
+  document.getElementById('fontsize').onchange = onfontchange;
+  document.getElementById('turbo').onchange = onturbo;
 };
 // when start putton clicked
 const onstart = () => {
-  "use strict";
+  'use strict';
   // disable the values when the animation is running
-  let animation = document.getElementById("animation");
-  document.getElementById("start").disabled = true;
-  document.getElementById("stop").disabled = false;
+  let animation = document.getElementById('animation');
+  document.getElementById('start').disabled = true;
+  document.getElementById('stop').disabled = false;
   animation.disabled = true;
   startanimation(animation.value);
 };
 //on stop animation button is clicked
 const onstop = () => {
-  "use strict";
+  'use strict';
   stopanimation();
 };
 // fire the animation
 const startanimation = (animation) => {
-  "use strict";
+  'use strict';
   frames = ANIMATIONS[animation];
-  frames = frames.split("=====\n");
+  frames = frames.split('=====\n');
   startInterval();
 };
 // clear interval and enable the buttons
 const stopanimation = () => {
-  "use strict";
-  document.getElementById("start").disabled = false;
-  document.getElementById("animation").disabled = false;
-  document.getElementById("stop").disabled = true;
-  document.getElementById("text-area").innerText = ANIMATIONS["Blank"];
+  'use strict';
+  document.getElementById('start').disabled = false;
+  document.getElementById('animation').disabled = false;
+  document.getElementById('stop').disabled = true;
+  document.getElementById('text-area').innerText = ANIMATIONS['Blank'];
   index = 0;
   clearInterval(intervalID);
   intervalID = 0;
 };
 // on font change, change text-area s font
 const onfontchange = (e) => {
-  "use strict";
-  document.getElementById("text-area").className = e.target.value;
+  'use strict';
+  document.getElementById('text-area').className = e.target.value;
 };
 
 //on click turbo change interval if the
 // interval already started turn off the last one and start new one
 const onturbo = () => {
-  "use strict";
-  if (document.getElementById("turbo").checked) {
+  'use strict';
+  if (document.getElementById('turbo').checked) {
     interVal = 50;
   } else {
     interVal = 250;
@@ -72,9 +72,9 @@ const onturbo = () => {
 };
 // start an interval and loop the frames on text area
 const startInterval = () => {
-  "use strict";
+  'use strict';
   intervalID = setInterval(() => {
-    document.getElementById("text-area").innerHTML = frames[index];
+    document.getElementById('text-area').innerHTML = frames[index];
     index = (index + 1) % frames.length;
   }, interVal);
 };
